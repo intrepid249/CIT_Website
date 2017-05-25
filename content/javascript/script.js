7955e6edae11dcf96ec5a6e93668accbb7107e0b
intrepid249 <jack.w.mccall@gmail.com>
2017-05-25 18:45:16 +1000

    Finished?

--- content/javascript/script.js
+++ content/javascript/script.js
@@ -13,12 +13,12 @@ function modGameInfo() {
 
 function purchase() {
 	var output = 
-	"Thank you for purchasing \t\t" + localStorage.getItem("name") + ".\n The amount of " + localStorage.getItem("price") +
+	"Thank you for purchasing " + "\t\t" + localStorage.getItem("name") + ".\n The amount of " + localStorage.getItem("price") +
 	" will be deducted from your acount.\n\n" +
 	"Please review the details below to make sure they are correct:\n\n" +
-	"Full name:\t\t" + document.getElementById("fname").value + " " document.getElementById("surname").value + "\n"
+	"Full name:\t\t" + document.getElementById("fname").value + " " + document.getElementById("surname").value + "\n" +
 	"Billing address:\t\t" + document.getElementById("address").value + document.getElementById("address2").value + " " + document.getElementById("city").value + 
-	document.getElementById("state").value + document.getElementById("postcode").value
+	document.getElementById("state").value + document.getElementById("postcode").value +
 	"Billing details:\n\tCard Number:\t\t" + document.getElementById("cardnum").value + "\n\tExpiry:\t\t" + document.getElementById("expdate").value + 
 	"\n\tCCV:\t\t" + document.getElementById("ccv");
 	
